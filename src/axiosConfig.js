12a7be5f6d7849c867d9205dbff43b5b2d3f7d75
AsTikiN <77283605+AsTikiN@users.noreply.github.com>
2023-05-20 19:25:34 +0300

    updated link

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:8000";
+export const baseUrl = "https://18fe-46-98-144-91.ngrok-free.app";
 export const drive = "/api/drive";
 export const user = "/api/user";
