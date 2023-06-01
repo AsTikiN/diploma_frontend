15d0712ea4467d119c18a08bf199f3f38a44c555
AsTikiN <77283605+AsTikiN@users.noreply.github.com>
2023-06-01 09:28:11 +0300

    changed link

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,3 +1,3 @@
-export const baseUrl = "https://596e-46-98-144-178.ngrok-free.app";
+export const baseUrl = "https://82f2-46-98-144-133.ngrok-free.app";
 export const drive = "/api/drive";
 export const user = "/api/user";
